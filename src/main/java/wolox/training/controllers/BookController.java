090d9d426641704b864bae16f5d226585ca131b2
IOliveto <ignacio.oliveto@wolox.com.ar>
2018-12-27 13:31:46 -0300

    Book Model and persistence added

--- src/main/java/wolox/training/controllers/BookController.java
+++ src/main/java/wolox/training/controllers/BookController.java
@@ -1,62 +1,23 @@
 package wolox.training.controllers;
 
-import wolox.training.DAO.BookDAO;
-//import wolox.training.exceptions.BookDoesNotExistException;
-import wolox.training.models.Book;
-import wolox.training.repositories.BookRepository;
-//import wolox.training.services.OpenLibraryService;
-import com.sun.deploy.net.HttpResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import wolox.training.repositories.BookRepository;
 
-import javax.net.ssl.HttpsURLConnection;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletResponse;
-import javax.xml.ws.Response;
-import java.time.LocalDate;
-import java.util.NoSuchElementException;
-import java.util.Optional;
-
-//@Controller
-@RequestMapping("/api/books")
 @RestController
+@RequestMapping("/api/books")
 public class BookController {
 
     @Autowired
     private BookRepository bookRepository;
 
-    //private OpenLibraryService onlineLibrary = new OpenLibraryService();
-
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
-        System.out.println(name);
-        model.addAttribute("name", name);
-        return "greeting " + name;
-    }
-
-    @GetMapping(value = {"/", ""})
-    public String home() {
-        return "home";
-    }
-
-    // Create
-    @RequestMapping("/create")
-    @PostMapping("/books/{id}")
-    @ResponseStatus(HttpStatus.CREATED)
-    public Book create(@RequestBody Book book ) {
-        return bookRepository.save(book);
-    }
-
-    // Read
-    @GetMapping("/view")
+    @GetMapping
     public Iterable findAll() {
         return bookRepository.findAll();
     }
-
-
-
 
 
 
--- src/main/java/wolox/training/repositories/BookRepository.java
+++ src/main/java/wolox/training/repositories/BookRepository.java
@@ -1,5 +1,6 @@
 package wolox.training.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import wolox.training.models.Book;
 import org.springframework.data.repository.*;
@@ -7,7 +8,7 @@ import org.springframework.data.repository.*;
 import java.util.Optional;
 
 
-public interface BookRepository extends CrudRepository<Book, Integer> {
+public interface BookRepository extends CrudRepository<Book, Long> {
 
     Optional<Book> findByAuthor(String author);
 
