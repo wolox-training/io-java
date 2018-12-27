26fe67dd4e20fbdbc40433b0ac3b7c8e2161b16f
IOliveto <ignacio.oliveto@wolox.com.ar>
2018-12-27 13:56:00 -0300

    Untrack ignored files!

--- src/main/java/wolox/training/controllers/BookController.java
+++ src/main/java/wolox/training/controllers/BookController.java
@@ -2,8 +2,10 @@ package wolox.training.controllers;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import wolox.training.repositories.BookRepository;
 
@@ -13,7 +15,11 @@ public class BookController {
 
     @Autowired
     private BookRepository bookRepository;
-
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
     @GetMapping
     public Iterable findAll() {
         return bookRepository.findAll();
