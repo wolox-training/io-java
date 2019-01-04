260be5cb68d05d668ececfd63b6209c42a99e5f6
IOliveto <ignacio.oliveto@wolox.com.ar>
2019-01-04 18:58:22 -0300

    Added user model an relation with book

--- src/main/java/wolox/training/models/User.java
+++ src/main/java/wolox/training/models/User.java
@@ -1,8 +1,9 @@
 package wolox.training.models;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 import java.util.Collection;
-import java.util.LinkedList;
+import java.util.List;
 
 @Entity
 public class User {
@@ -12,19 +13,76 @@ public class User {
     private long id;
 
     @Column(nullable = false, unique = true)
-    private String Username;
+    private String username;
 
     @Column(nullable = false, unique = true)
-    private String Name;
+    private String name;
 
     @Column(nullable = false, unique = true)
-    private String Birthdate;
+    private LocalDate birthdate;
 
-    @Column
-    @ManyToMany(cascade = {CascadeType.ALL})
-    private Collection<Book> books = new LinkedList<Book>();
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "book_user",
+            joinColumns = @JoinColumn(name = "book_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id",
+                    referencedColumnName = "id"))
+    private List<Book> books;
 
+    public User() {
+    }
 
+    public long getId() {
+        return id;
+    }
 
+    public void setId(long id) {
+        this.id = id;
+    }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
